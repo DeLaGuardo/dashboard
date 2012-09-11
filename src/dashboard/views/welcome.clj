8b5d77d4c4f6f60769b4535dd158665f2ee49d92
DeLaGuardo <delaguardo@gmail.com>
2012-09-11 15:47:00 +0400

    References #2. init: created new project with lein-noir template

---
+++ src/dashboard/views/welcome.clj
@@ -0,0 +1,8 @@
+(ns dashboard.views.welcome
+  (:require [dashboard.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to dashboard"]))
